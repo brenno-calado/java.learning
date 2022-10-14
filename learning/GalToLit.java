fc3c2a59be8a5593b84f2ba3ee8d24b751c680a2
Brenno Calado <brenno.calado@gmail.com>
2022-10-14 12:24:25 -0300

    feat - GalToLit

---
+++ learning/GalToLit.java
@@ -0,0 +1,10 @@
+class GalToLit {
+	public static void main(String args[]) {
+		double gallons = 13;
+		double liters;
+
+		liters = gallons * 3.7854;
+
+		System.out.println(gallons + " gallons is " + liters + " liters.");
+	}
+}
