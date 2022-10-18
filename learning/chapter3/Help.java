9c3c9c309b8f6aa0da6af9b1413897152c006ead
Brenno Calado <brenno.calado@gmail.com>
2022-10-17 21:41:17 -0300

    feat - help.java

--- learning/chapter3/Help.java
+++ learning/chapter3/Help.java
@@ -1,6 +1,22 @@
 package chapter3;
+
 class Help {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws java.io.IOException {
+        char ch;
+
+        System.out.println("Need some help?\n1. if\n2. switch");
+        System.out.print("Press a number to get a hint: ");
+        ch = (char) System.in.read();
+
+        switch (ch) {
+            case '1':
+                System.out.println("if (condition) {\n\tvalue\n}");
+                break;
+            case '2':
+                System.out.println("switch (key) {\n\tcase value:\n\t\n\t\tbreak;\n\t\n\tdefault:\n\t\tbreak;\n}");
+                break;
+            default:
+                break;
+        }
     }
 }
